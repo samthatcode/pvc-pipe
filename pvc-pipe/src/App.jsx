27813841d7905d1929da4f2d0453fafac1970b9a
samthatcode <samthatcode@gmail.com>
2023-03-05 17:22:55 +0100

    Updated DeliveryInProgress and functionality

--- pvc-pipe/src/App.jsx
+++ pvc-pipe/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { Home } from './pages'
+import { Home } from "./pages";
 import { DeliveryInProgress } from "./pages";
+import { personDetails } from "./data/index.js";
 
 function App() {
   const [user, setUser] = useState("samthatcode");
+  const [personDetailsDeliveryInput, setpersonDetailsDeliveryInput] = useState(
+    {}
+  );
+
+  useEffect(() => {
+    setpersonDetailsDeliveryInput(personDetails);
+  }, []);
 
   return (
     <div className="font-poppins">
-      <Home />
-      {/* <DeliveryInProgress /> */}
+      {/* <Home /> */}
+
+      <DeliveryInProgress person={personDetailsDeliveryInput} />
     </div>
   );
 }
--- pvc-pipe/src/pages/DeliveryInProgress.jsx
+++ pvc-pipe/src/pages/DeliveryInProgress.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import person1 from "../assets/images/person1.jpg";
 
 import { Navbar, Footer } from "../components";
 
-const DeliveryInProgress = () => {
+const DeliveryInProgress = ({ person }) => {
   return (
     <>
       <Navbar />
 
       <div className="h-56 w-auto bg-lightYellow pt-20 ">
         <img
-          src={person1}
+          src={person?.photo}
           alt="Person"
           className="ml-20 mt-14 h-40 w-40 rounded-full border-4  border-green-500 "
         />
@@ -22,7 +21,7 @@ const DeliveryInProgress = () => {
             Delivery in Progress
           </div>
           <div className="text-[#01301D]">
-            VIN no: <span className=" text-primary">1239087444</span>
+            VIN no: <span className=" text-primary">{person.vinNo}</span>
           </div>
         </div>
 
@@ -30,100 +29,128 @@ const DeliveryInProgress = () => {
 
         <div className="container mx-auto">
           <div className=" m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
-            <label className="">
-              <span className=" text-sm font-medium text-slate-700">
+            <div className="">
+              <label
+                for="firstname"
+                className=" text-sm font-medium text-slate-700"
+              >
                 FIRST NAME:
-              </span>
+              </label>
               <input
+                id="fname"
                 type="text"
-                placeholder="Peace"
+                value={person.firstName}
                 className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
               />
-            </label>
-            <label className="">
-              <span className="  text-sm font-medium text-slate-700">
+            </div>
+            <div className="">
+              <label
+                for="lastname"
+                className="text-sm font-medium text-slate-700"
+              >
                 LAST NAME:
-              </span>
+              </label>
               <input
+                id="lname"
                 type="text"
-                placeholder="Emmanuel"
+                value={person.lastName}
                 className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
               />
-            </label>
+            </div>
           </div>
           <div className="m-7 ">
-            <label className="">
-              <span class=" mr-2 text-sm font-medium text-slate-700">
+            <div className="">
+              <label
+                for="phone"
+                class=" mr-2 text-sm font-medium text-slate-700"
+              >
                 PHONE NO:
-              </span>
+              </label>
               <br />
               <input
-                type="number"
-                placeholder="081-55564-466"
+                id="pnumber"
+                type="text"
+                value={person.phoneNumber}
                 className="mt-1 w-[100%] rounded-md  border border-slate-300 bg-textInputColor px-3 py-2  text-sm placeholder-slate-400 shadow-sm outline-none lg:w-[66%]"
               />
-            </label>
+            </div>
           </div>
           <div className="m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
-            <label className="">
-              <span className=" text-sm font-medium text-slate-700">
+            <div className="">
+              <label
+                for="state"
+                className=" text-sm font-medium text-slate-700"
+              >
                 STATE:
-              </span>
+              </label>
               <input
+                id="state"
                 type="text"
-                placeholder="Lagos State"
+                value={person.state}
                 className="mt-1 w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
               />
-            </label>
-            <label className="">
-              <span className=" text-sm font-medium text-slate-700">
+            </div>
+            <div className="">
+              <label for="lga" className="text-sm font-medium text-slate-700">
                 L.G.A:
-              </span>
+              </label>
               <input
+                id="lga"
                 type="text"
-                placeholder="Ikorodu"
+                value={person.lga}
                 className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
               />
-            </label>
+            </div>
           </div>
           <div>
-            {" "}
             <div className="m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
-              <label className="">
-                <span className=" text-sm font-medium text-slate-700">
+              <div className="">
+                <label
+                  for="code"
+                  className=" text-sm font-medium text-slate-700"
+                >
                   CODE:
-                </span>
+                </label>
                 <input
-                  type="number"
-                  placeholder="16-27-02-004"
+                  id="code"
+                  type="text"
+                  value={person.code}
                   className="mt-1 w-full rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
                 />
-              </label>
-              <label className="">
-                <span className=" text-sm font-medium text-slate-700">
+              </div>
+              <div className="">
+                <label
+                  for="dob"
+                  className=" text-sm font-medium text-slate-700"
+                >
                   DATE OF BIRTH:
-                </span>
+                </label>
                 <input
-                  type="number"
-                  placeholder="12-02-1990"
+                  id="dob"
+                  type="text"
+                  value={person.dob}
                   className="mt-1 w-[100%]  rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
                 />
-              </label>
+              </div>
             </div>
           </div>
 
           <div className="m-7">
-            <label className="">
-              <span className=" text-sm font-medium text-slate-700">
+            <div className="">
+              <label
+                for="pickup"
+                className=" text-sm font-medium text-slate-700"
+              >
                 PICK-UP LOCATION:
-              </span>
+              </label>
               <br />
               <input
+                id="pickup"
                 type="text"
-                placeholder="Lagos State"
+                value={person.pickupLocation}
                 className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3 py-2 text-sm placeholder-slate-400  shadow-sm outline-none lg:w-[66%]"
               />
-            </label>
+            </div>
           </div>
           <div className="m-7 pt-10">
             <button className="w-full rounded border bg-primary p-3 text-center text-white transition duration-200 hover:scale-95 lg:w-[66%]">
@@ -135,6 +162,111 @@ const DeliveryInProgress = () => {
       </div>
 
       <Footer />
+
+      {/* <div className=" m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
+            <div for="firstname" className="">
+              <label className=" text-sm font-medium text-slate-700">
+                FIRST NAME:
+              </label>
+              <input
+                id="fname"
+                type="text"
+                placeholder="Peace"
+                className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+              />
+            </div>
+            <div for="lastname" className="">
+              <label className="  text-sm font-medium text-slate-700">
+                LAST NAME:
+              </label>
+              <input
+                id="lname"
+                type="text"
+                placeholder="Emmanuel"
+                className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+              />
+            </div>
+          </div>
+          <div className="m-7 ">
+            <div for="phone" className="">
+              <label class=" mr-2 text-sm font-medium text-slate-700">
+                PHONE NO:
+              </label>
+              <br />
+              <input
+                id="pnumber"
+                type="number"
+                placeholder="081-55564-466"
+                className="mt-1 w-[100%] rounded-md  border border-slate-300 bg-textInputColor px-3 py-2  text-sm placeholder-slate-400 shadow-sm outline-none lg:w-[66%]"
+              />
+            </div>
+          </div>
+          <div className="m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
+            <div for="state" className="">
+              <label className=" text-sm font-medium text-slate-700">
+                STATE:
+              </label>
+              <input
+                id="state"
+                type="text"
+                placeholder="Lagos State"
+                className="mt-1 w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+              />
+            </div>
+            <div for="lga" className="">
+              <label className=" text-sm font-medium text-slate-700">
+                L.G.A:
+              </label>
+              <input
+                id="lga"
+                type="text"
+                placeholder="Ikorodu"
+                className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+              />
+            </div>
+          </div>
+          <div>
+            {" "}
+            <div className="m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
+              <div for="code" className="">
+                <label className=" text-sm font-medium text-slate-700">
+                  CODE:
+                </label>
+                <input
+                  id="code"
+                  type="number"
+                  placeholder="16-27-02-004"
+                  className="mt-1 w-full rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+                />
+              </div>
+              <div for="dob" className="">
+                <label className=" text-sm font-medium text-slate-700">
+                  DATE OF BIRTH:
+                </label>
+                <input
+                  id="dob"
+                  type="number"
+                  placeholder="12-02-1990"
+                  className="mt-1 w-[100%]  rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="m-7">
+            <div for="pickup" className="">
+              <label className=" text-sm font-medium text-slate-700">
+                PICK-UP LOCATION:
+              </label>
+              <br />
+              <input
+                id="pickup"
+                type="text"
+                placeholder="Lagos State"
+                className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3 py-2 text-sm placeholder-slate-400  shadow-sm outline-none lg:w-[66%]"
+              />
+            </div>
+          </div> */}
     </>
   );
 };
