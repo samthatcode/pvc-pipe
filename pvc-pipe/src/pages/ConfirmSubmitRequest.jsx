ee10b3ada47b0152dffda9b5b5590dd5872451a6
samthatcode <samthatcode@gmail.com>
2023-03-08 11:25:22 +0100

    Fixed and updated DeliveryInProg responsiveness & ConfirmSubmitRequest page

--- pvc-pipe/src/pages/ConfirmSubmitRequest.jsx
+++ pvc-pipe/src/pages/ConfirmSubmitRequest.jsx
@@ -1,101 +1,99 @@
 import React from "react";
-import { Navbar, Footer } from "../components";
+import { Layout } from "../components";
 
 const ConfirmSubmitRequest = ({ person }) => {
   return (
-    <div>
-      <Navbar />
-
-      <div className="container mx-auto  mb-40 bg-inherit px-4 pt-36 sm:px-6 lg:px-8">
-        <div className="mx-auto grid grid-cols-1">
-          <div className=" mb-3 font-poppins text-2xl font-semibold">
-            Please confirm and submit your request
-          </div>
-          <div className=" mb-8 font-poppins text-sm font-normal text-[#01301D80]">
-            <div className="">
-              By clicking confirm order, you are agreeing to PVC PIPE's terms
-              and conditions.
+    <Layout>
+      <div>
+        <div className="container mx-auto  mb-40 bg-inherit px-4 pt-36 sm:px-6 lg:px-8">
+          <div className="mx-auto grid grid-cols-1">
+            <div className=" mb-3 font-poppins text-2xl font-semibold">
+              Please confirm and submit your request
             </div>
-            <div className="">
-              You are also confirming that all details are accurate
-            </div>
-          </div>
-          <div className="max-w-640 container mb-5 h-auto w-full overflow-hidden rounded-md border border-[#80978E] p-5">
-            <div className="mb-4 flex justify-between ">
-              <div className="font-poppins text-xl font-bold">
-                Shipping Address
+            <div className=" mb-8 font-poppins text-sm font-normal text-[#01301D80]">
+              <div className="">
+                By clicking confirm order, you are agreeing to PVC PIPE's terms
+                and conditions.
               </div>
-              <div className="text-sm text-secondary">Edit</div>
-            </div>
-            <div className="">
-              <div className="text-sm text-[#01301D80]">{person.address}</div>
-              <div className="mt-9 text-primary">{person.phoneNumber}</div>
-            </div>
-          </div>
-          <div className="max-w-640 container mb-5 h-auto w-full overflow-hidden rounded-md border border-[#80978E] p-5">
-            <div className="mb-4 flex justify-between ">
-              <div className="font-poppins text-xl font-bold">
-                PVC Requested
-              </div>
-              <div className="text-sm text-secondary">Edit</div>
-            </div>
-            <div className="">
-              <div className="text-sm  text-[#01301D80]">
-                <div className="flex gap-0.5">
-                  <div className="">Name:</div>
-                  <div className="text-l ml-10 font-poppins font-bold text-primary">
-                    {person.firstName} {person.lastName}
-                  </div>
-                </div>
-                <div className="flex gap-4">
-                  <div className="">Sex:</div>
-                  <div className="text-l ml-10 font-poppins font-bold text-primary">
-                    {person.sex}
-                  </div>
-                </div>
-                <div className="flex">
-                  <div className="">VIN no:</div>
-                  <div className="text-l ml-9 font-poppins font-bold text-primary">
-                    {person.vinNo}
-                  </div>
-                </div>
-                <div className="flex gap-1">
-                  <div className="">L.G.A:</div>
-                  <div className="text-l ml-10 font-poppins font-bold text-primary">
-                    {person.lga}
-                  </div>
-                </div>
-                <div className="flex gap-1">
-                  <div className="">Phone no:</div>
-                  <div className="text-l ml-4 font-poppins font-bold text-primary">
-                    {person.phoneNumber}
-                  </div>
-                </div>
+              <div className="">
+                You are also confirming that all details are accurate
               </div>
             </div>
-          </div>
-          <div className="max-w-640 container mb-5 h-auto w-full overflow-hidden rounded-md border border-[#80978E] p-5">
-            <div className="mb-4 flex justify-between ">
-              <div className="font-poppins text-xl font-bold">
-                Payment Method
+            <div className="max-w-640 container mb-5 h-auto w-full overflow-hidden rounded-md border border-[#80978E] p-5">
+              <div className="mb-4 flex justify-between ">
+                <div className="font-poppins text-xl font-bold">
+                  Shipping Address
+                </div>
+                <div className="text-sm text-secondary">Edit</div>
               </div>
-              <div className="text-sm text-secondary">Edit</div>
+              <div className="">
+                <div className="text-sm text-[#01301D80]">{person.address}</div>
+                <div className="mt-9 text-primary">{person.phoneNumber}</div>
+              </div>
             </div>
-            <div className="flex gap-40">
-              <div className="text-sm text-[#01301D80]">Cash on delivery</div>
-              <div className="text-sm font-semibold text-primary">#2500</div>
+            <div className="max-w-640 container mb-5 h-auto w-full overflow-hidden rounded-md border border-[#80978E] p-5">
+              <div className="mb-4 flex justify-between ">
+                <div className="font-poppins text-xl font-bold">
+                  PVC Requested
+                </div>
+                <div className="text-sm text-secondary">Edit</div>
+              </div>
+              <div className="">
+                <div className="text-sm  text-[#01301D80]">
+                  <div className="flex ">
+                    <div className="">Name:</div>
+                    <div className="text-l ml-9 font-poppins font-bold text-primary">
+                      {person.firstName} {person.lastName}
+                    </div>
+                  </div>
+                  <div className="flex gap-3.5">
+                    <div className="">Sex:</div>
+                    <div className="text-l ml-10 font-poppins font-bold text-primary">
+                      {person.sex}
+                    </div>
+                  </div>
+                  <div className="gap-.5 flex">
+                    <div className="">VIN no:</div>
+                    <div className="text-l ml-8 font-poppins font-bold text-primary">
+                      {person.vinNo}
+                    </div>
+                  </div>
+                  <div className="flex gap-1">
+                    <div className="">L.G.A:</div>
+                    <div className="text-l ml-10 font-poppins font-bold text-primary">
+                      {person.lga}
+                    </div>
+                  </div>
+                  <div className="flex gap-0">
+                    <div className="">Phone no:</div>
+                    <div className="text-l ml-3 font-poppins font-bold text-primary">
+                      {person.phoneNumber}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="mt-5">
-            <button className="w-full rounded border bg-primary p-3 text-center text-white transition duration-200 hover:scale-95 lg:w-full">
-              Confirm & Submit
-            </button>
+            <div className="max-w-640 container mb-5 h-auto w-full overflow-hidden rounded-md border border-[#80978E] p-5">
+              <div className="mb-4 flex justify-between ">
+                <div className="font-poppins text-xl font-bold">
+                  Payment Method
+                </div>
+                <div className="text-sm text-secondary">Edit</div>
+              </div>
+              <div className="flex gap-40">
+                <div className="text-sm text-[#01301D80]">Cash on delivery</div>
+                <div className="text-sm font-semibold text-primary">#2500</div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <button className="w-full rounded border bg-primary p-3 text-center text-white transition duration-200 hover:scale-95 lg:w-full">
+                Confirm & Submit
+              </button>
+            </div>
           </div>
         </div>
       </div>
-
-      <Footer />
-    </div>
+    </Layout>
   );
 };
 
--- pvc-pipe/src/pages/DeliveryInProgress.jsx
+++ pvc-pipe/src/pages/DeliveryInProgress.jsx
@@ -1,169 +1,166 @@
 import React from "react";
-
+import { Layout } from "../components";
 import { Navbar, Footer } from "../components";
 
 const DeliveryInProgress = ({ person }) => {
   return (
-    <>
-      <Navbar />
-
-      <div className="h-56 w-auto bg-lightYellow pt-20 ">
-        <img
-          src={person?.photo}
-          alt="Person"
-          className="ml-20 mt-14 h-40 w-40 rounded-full border-4  border-green-500 "
-        />
-      </div>
-
-      <div className="bg-inherit">
-        <div className="ml-72 mb-10 font-poppins font-semibold">
-          <div className="mb-4 mt-4 text-2xl text-[#10442F]">
-            Delivery in Progress
-          </div>
-          <div className="text-[#01301D]">
-            VIN no: <span className=" text-primary">{person.vinNo}</span>
-          </div>
+    <Layout>
+      <div className="overflow-hidden">
+        <div className="h-56 w-auto bg-lightYellow pt-20 ">
+          <img
+            src={person?.photo}
+            alt="Person"
+            className="ml-20 mt-14 h-40 w-40 rounded-full border-4  border-green-500 "
+          />
         </div>
 
-        {/* Input feild */}
+        <div className="bg-inherit">
+          <div className="ml-60  mb-10 mt-14 font-poppins font-semibold">
+            <div className="mb-4 mt-4 text-2xl text-[#10442F]">
+              Delivery in Progress
+            </div>
+            <div className="text-[#01301D]">
+              VIN no: <span className=" text-primary">{person.vinNo}</span>
+            </div>
+          </div>
 
-        <div className="container mx-auto">
-          <div className=" m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
-            <div className="">
-              <label
-                for="firstname"
-                className=" text-sm font-medium text-slate-700"
-              >
-                FIRST NAME:
-              </label>
-              <input
-                id="fname"
-                type="text"
-                value={person.firstName}
-                className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
-              />
+          {/* Input feild */}
+
+          <div className="container mx-auto">
+            <div className=" m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
+              <div className="">
+                <label
+                  for="firstname"
+                  className=" text-sm font-medium text-slate-700"
+                >
+                  FIRST NAME:
+                </label>
+                <input
+                  id="fname"
+                  type="text"
+                  value={person.firstName}
+                  className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+                />
+              </div>
+              <div className="">
+                <label
+                  for="lastname"
+                  className="text-sm font-medium text-slate-700"
+                >
+                  LAST NAME:
+                </label>
+                <input
+                  id="lname"
+                  type="text"
+                  value={person.lastName}
+                  className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+                />
+              </div>
             </div>
-            <div className="">
-              <label
-                for="lastname"
-                className="text-sm font-medium text-slate-700"
-              >
-                LAST NAME:
-              </label>
-              <input
-                id="lname"
-                type="text"
-                value={person.lastName}
-                className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
-              />
+            <div className="m-7 ">
+              <div className="">
+                <label
+                  for="phone"
+                  class=" mr-2 text-sm font-medium text-slate-700"
+                >
+                  PHONE NO:
+                </label>
+                <br />
+                <input
+                  id="pnumber"
+                  type="text"
+                  value={person.phoneNumber}
+                  className="mt-1 w-[100%] rounded-md  border border-slate-300 bg-textInputColor px-3 py-2  text-sm placeholder-slate-400 shadow-sm outline-none lg:w-[66%]"
+                />
+              </div>
             </div>
-          </div>
-          <div className="m-7 ">
-            <div className="">
-              <label
-                for="phone"
-                class=" mr-2 text-sm font-medium text-slate-700"
-              >
-                PHONE NO:
-              </label>
-              <br />
-              <input
-                id="pnumber"
-                type="text"
-                value={person.phoneNumber}
-                className="mt-1 w-[100%] rounded-md  border border-slate-300 bg-textInputColor px-3 py-2  text-sm placeholder-slate-400 shadow-sm outline-none lg:w-[66%]"
-              />
-            </div>
-          </div>
-          <div className="m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
-            <div className="">
-              <label
-                for="state"
-                className=" text-sm font-medium text-slate-700"
-              >
-                STATE:
-              </label>
-              <input
-                id="state"
-                type="text"
-                value={person.state}
-                className="mt-1 w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
-              />
-            </div>
-            <div className="">
-              <label for="lga" className="text-sm font-medium text-slate-700">
-                L.G.A:
-              </label>
-              <input
-                id="lga"
-                type="text"
-                value={person.lga}
-                className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
-              />
-            </div>
-          </div>
-          <div>
             <div className="m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
               <div className="">
                 <label
-                  for="code"
+                  for="state"
                   className=" text-sm font-medium text-slate-700"
                 >
-                  CODE:
+                  STATE:
                 </label>
                 <input
-                  id="code"
+                  id="state"
                   type="text"
-                  value={person.code}
-                  className="mt-1 w-full rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+                  value={person.state}
+                  className="mt-1 w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
                 />
               </div>
               <div className="">
-                <label
-                  for="dob"
-                  className=" text-sm font-medium text-slate-700"
-                >
-                  DATE OF BIRTH:
+                <label for="lga" className="text-sm font-medium text-slate-700">
+                  L.G.A:
                 </label>
                 <input
-                  id="dob"
+                  id="lga"
                   type="text"
-                  value={person.dob}
-                  className="mt-1 w-[100%]  rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+                  value={person.lga}
+                  className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
                 />
               </div>
             </div>
-          </div>
-
-          <div className="m-7">
-            <div className="">
-              <label
-                for="pickup"
-                className=" text-sm font-medium text-slate-700"
-              >
-                PICK-UP LOCATION:
-              </label>
-              <br />
-              <input
-                id="pickup"
-                type="text"
-                value={person.pickupLocation}
-                className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3 py-2 text-sm placeholder-slate-400  shadow-sm outline-none lg:w-[66%]"
-              />
+            <div>
+              <div className="m-7 grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3">
+                <div className="">
+                  <label
+                    for="code"
+                    className=" text-sm font-medium text-slate-700"
+                  >
+                    CODE:
+                  </label>
+                  <input
+                    id="code"
+                    type="text"
+                    value={person.code}
+                    className="mt-1 w-full rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+                  />
+                </div>
+                <div className="">
+                  <label
+                    for="dob"
+                    className=" text-sm font-medium text-slate-700"
+                  >
+                    DATE OF BIRTH:
+                  </label>
+                  <input
+                    id="dob"
+                    type="text"
+                    value={person.dob}
+                    className="mt-1 w-[100%]  rounded-md border border-slate-300 bg-textInputColor px-3  py-2 text-sm placeholder-slate-400 shadow-sm outline-none"
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="m-7 pt-10">
-            <button className="w-full rounded border bg-primary p-3 text-center text-white transition duration-200 hover:scale-95 lg:w-[66%]">
-              Track Delivery
-            </button>
-            <p className="text-sm">Click to track delivery progress</p>
+
+            <div className="m-7">
+              <div className="">
+                <label
+                  for="pickup"
+                  className=" text-sm font-medium text-slate-700"
+                >
+                  PICK-UP LOCATION:
+                </label>
+                <br />
+                <input
+                  id="pickup"
+                  type="text"
+                  value={person.pickupLocation}
+                  className="mt-1  w-[100%] rounded-md border border-slate-300 bg-textInputColor px-3 py-2 text-sm placeholder-slate-400  shadow-sm outline-none lg:w-[66%]"
+                />
+              </div>
+            </div>
+            <div className="m-7 pt-10">
+              <button className="w-full rounded border bg-primary p-3 text-center text-white transition duration-200 hover:scale-95 lg:w-[66%]">
+                Track Delivery
+              </button>
+              <p className="text-sm">Click to track delivery progress</p>
+            </div>
           </div>
         </div>
       </div>
-
-      <Footer />
-
-    </>
+    </Layout>
   );
 };
 
