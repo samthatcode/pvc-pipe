69def6f6ead0b57e302cc823fd9496724102b6bc
Oladimeji Abdrahman Adebayo <abdrahmanoladimeji02@gmail.com>
2023-03-13 18:43:36 +0100

    Update: added rollup module

--- pvc-pipe/rollup.config.js
+++ pvc-pipe/rollup.config.js
@@ -2,7 +2,7 @@
 export default {
     input: 'src/index.js',
     output: {
-      file: 'dist/bundle.js',
+      file: 'pvc-pipe/dist/bundle.js',
       format: 'cjs'
     },
     external: ['react-paystack'] // Add the external module here
