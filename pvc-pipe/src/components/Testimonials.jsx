8f754eb341063eb5ada44f4f23d9679f5898cc40
samthatcode <samthatcode@gmail.com>
2023-03-14 21:02:52 +0100

    .

--- pvc-pipe/src/components/Testimonials.jsx
+++ pvc-pipe/src/components/Testimonials.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import test1 from "./../assets/images/testimonial1.png";
-import arrow from "./../assets/icons/arrow.svg";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -17,7 +16,7 @@ const Testimonials = () => {
     slidesToScroll: 1,
     dots: true,
     appendDots: (dots) => (
-      <div>
+      <div className="text-2xl font-semibold ">
         <ul style={{ margin: "0px", fontSize: "35px" }}> {dots} </ul>
       </div>
     ),
@@ -25,6 +24,15 @@ const Testimonials = () => {
     responsive: [
       {
         breakpoint: 1024,
+        settings: {
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
         settings: {
           infinite: true,
           slidesToShow: 2,
@@ -36,7 +44,7 @@ const Testimonials = () => {
         breakpoint: 600,
         settings: {
           infinite: true,
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
           dots: true,
         },
@@ -103,7 +111,7 @@ const Testimonials = () => {
           className=" center-padding"
         >
           {testimonials.map((testimonial) => (
-            <div key={testimonial.id} className="px-4 sm:px-8 md:px-12">
+            <div key={testimonial.id} className="px-2 sm:px-5 md:px-4">
               <div className="flex flex-col ">
                 <img
                   src={testimonial.image}
@@ -116,51 +124,14 @@ const Testimonials = () => {
                 <div className="text-[1.25rem] font-medium">
                   {testimonial.name}
                 </div>
-                <div className="text-lg font-[400] pb-5">{testimonial.state}</div>
+                <div className="mb- text-lg font-[400]">
+                  {testimonial.state}
+                </div>
               </div>
             </div>
           ))}
         </Slider>
       </div>
-      {/* <div className="relative mx-12 grid grid-cols-1 gap-8 md:mx-28 md:grid-cols-2">
-        <div className="absolute top-1/2 -left-20 hidden rotate-180 cursor-pointer hover:scale-90 active:scale-100 md:flex">
-          <img src={arrow} alt="Prev" />
-        </div>
-        <div className="absolute top-1/2 -right-20 hidden cursor-pointer hover:scale-90 active:scale-100 md:flex">
-          <img src={arrow} alt="Next" />
-        </div>
-        <div className="-mb-5 md:mb-0">
-          <img src={test1} alt="Testimonial" />
-        </div>
-        <div className="-mt-40  -mb-5 flex justify-between  md:hidden">
-          <div className="-ml-11  ">
-            <img
-              src={arrow}
-              alt="Prev"
-              className=" rotate-180 cursor-pointer hover:scale-90 active:scale-100 "
-            />
-          </div>
-          <div className="-mr-11  ">
-            <img
-              src={arrow}
-              alt="Next"
-              className="  cursor-pointer hover:scale-90 active:scale-100"
-            />
-          </div>
-        </div>
-
-        <div className="-mt-4 md:mt-0">
-          <div className="text-lg">
-            I ordered my mum’s PVC and mine and within 48 hours it got to us. so
-            glad i did not have to wait in that long queue at ikeja.
-            <br /> Thank you PVC PIPE.
-          </div>
-          <div>
-            <div className="text-[1.25rem] font-medium">Loretta Igwe</div>
-            <div className="text-lg">Lagos State</div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
